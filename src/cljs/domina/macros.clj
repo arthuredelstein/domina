30c92be0cab932300a5c68104f34d4ba5d8ec98d
Arthur Edelstein <arthuredelstein@gmail.com>
2012-06-20 02:40:16 -0700

    Update master

--- src/cljs/domina/macros.clj
+++ src/cljs/domina/macros.clj
@@ -10,7 +10,7 @@
 (defmacro satisfies?
   "Returns true if x satisfies the protocol. This is entirely a copy of the default satisfies implementation, except that it checks if the invokee has the 'hasOwnProperty' method: some JS objects, particularly in older browsers, don't."
   [psym x]
-  (let [p (:name (cljs.compiler/resolve-var (dissoc &env :locals) psym))
+  (let [p (:name (cljs.analyzer/resolve-var (dissoc &env :locals) psym))
         prefix (protocol-prefix p)]
     `(let [x# ~x]
        (if (and x#
